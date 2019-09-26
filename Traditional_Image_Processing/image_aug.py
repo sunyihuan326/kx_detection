b515ec7659d54bb3f9a88a4360bb905c2a3e178b
sunyihuan_joy <1336840590@qq.com>
2019-09-26 17:04:59 +0800

    traditional: data aug

--- Traditional_Image_Processing/image_aug.py
+++ Traditional_Image_Processing/image_aug.py
@@ -6,7 +6,7 @@
 @Time    : 2019/9/24 16:29
 @Author  : sunyihuan
 """
-from PIL import Image, ImageFilter, ImageEnhance
+from PIL import Image, ImageFilter, ImageEnhance, ImageDraw
 from skimage import util
 import numpy as np
 import matplotlib.pyplot as plt
@@ -14,6 +14,11 @@ import matplotlib.pyplot as plt
 img_path = "C:/Users/sunyihuan/Desktop/test/3.jpg"
 
 im = Image.open(img_path)
+
+# 图片中画线
+draw_img = ImageDraw.Draw(im)
+draw_img.line((0, 400, 800, 400), fill=(34, 139, 34))
+im.show()
 # blurf = im.filter(ImageFilter.BLUR)  #模糊滤波
 # blurf = im.filter(ImageFilter.EDGE_ENHANCE)  # 边界增强滤波，其他滤波：
 #  SHARPEN、MaxFilter、MedianFilter、SMOOTH_MORE、EDGE_ENHANCE_MORE等
@@ -31,7 +36,7 @@ tmp = ImageEnhance.Brightness(im)
 img_bright = tmp.enhance(1.5)
 # img_bright.show()
 
-im = np.array(im)
+# im = np.array(im)
 # noise_gs_img = util.random_noise(im, mode="gaussian")  # 加入高斯噪声
 # noise_gs_img = util.random_noise(im, mode="salt")  #加入椒盐噪声，其他噪声方法：localvar、poisson、pepper、s&p、speckle
 # plt.figure()
@@ -39,11 +44,11 @@ im = np.array(im)
 # plt.show()
 
 # 底部白条
-ones = np.ones((600, 800, 3))
-sliced = [[255 for i in range(3)] for j in range(800)]
-sliced = np.array([sliced for k in range(30)])
-im[570:, :, :] = sliced
-img = im.astype(int)
+# ones = np.ones((600, 800, 3))
+# sliced = [[255 for i in range(3)] for j in range(800)]
+# sliced = np.array([sliced for k in range(30)])
+# im[570:, :, :] = sliced
+# img = im.astype(int)
 #
 #
 # plt.figure()
@@ -52,13 +57,13 @@ img = im.astype(int)
 
 
 # 图片中加入水雾效果
-import cv2
-img1 = cv2.imread(img_path)
-img2 = cv2.imread('C:/Users/sunyihuan/Desktop/wuzi1.jpg')
-img2 = cv2.resize(img2, (800, 600))  # 统一图片大小
-# dst=cv2.add(img1,img2)
-dst = cv2.addWeighted(img1, 0.7, img2, 0.3, 0)
-
-cv2.imshow('dst', dst)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+# import cv2
+# img1 = cv2.imread(img_path)
+# img2 = cv2.imread('C:/Users/sunyihuan/Desktop/wuzi1.jpg')
+# img2 = cv2.resize(img2, (800, 600))  # 统一图片大小
+# # dst=cv2.add(img1,img2)
+# dst = cv2.addWeighted(img1, 0.7, img2, 0.3, 0)
+#
+# cv2.imshow('dst', dst)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
---
+++ Traditional_Image_Processing/img_aug_drawline.py
@@ -0,0 +1,55 @@
+# -*- encoding: utf-8 -*-
+
+"""
+模拟可能出现的问题，图片中画直线
+@File    : img_aug_drawline.py
+@Time    : 2019/9/26 16:34
+@Author  : sunyihuan
+"""
+
+from PIL import Image, ImageDraw
+import numpy as np
+import os
+from tqdm import tqdm
+import shutil
+import matplotlib.pyplot as plt
+
+
+def aug_drawLine(img_path):
+    '''
+    图片中加入白条
+    :param img_path: 图片地址
+    :return:
+    '''
+    im = Image.open(img_path)
+    draw_img = ImageDraw.Draw(im)
+    draw_img.line((0, 400, 800, 400), fill=(34, 139, 34))
+    return np.array(im)
+
+
+def data_aug(img_dir, xml_dir, img_save_dir, xml_save_dir):
+    '''
+    图像增强后保存
+    :param img_dir: 原图片地址
+    :param xml_dir: xml地址
+    :param img_save_dir: 增强后图片保存地址
+    :param xml_save_dir: 增强后xml保存地址
+    :return:
+    '''
+    for img_file in tqdm(os.listdir(img_dir)):
+        img_path = img_dir + "/" + img_file
+        img = aug_drawLine(img_path)
+        img_name = str(img_file).split(".")[0] + "_white" + ".jpg"  # 图片名称
+        plt.imsave(img_save_dir + "/" + img_name, img.astype(np.uint8))  # 保存图片
+        xml_name = str(img_name).split(".")[0] + ".xml"  # xml文件名称
+        shutil.copy(xml_dir + "/" + str(img_file).split(".")[0] + ".xml", xml_save_dir + "/" + xml_name)  # 拷贝xml数据
+
+
+if __name__ == "__main__":
+    img_dir = "E:/DataSets/KX_FOODSets0802/JPGImages/SweetPotatoM"
+    xml_dir = "E:/DataSets/KX_FOODSets0802/Anotations/SweetPotatoM"
+    img_save_dir = "C:/Users/sunyihuan/Desktop/data/imgs"
+    xml_save_dir = "C:/Users/sunyihuan/Desktop/data/annotations"
+    if not os.path.exists(img_save_dir): os.mkdir(img_save_dir)
+    if not os.path.exists(xml_save_dir): os.mkdir(xml_save_dir)
+    data_aug(img_dir, xml_dir, img_save_dir, xml_save_dir)
